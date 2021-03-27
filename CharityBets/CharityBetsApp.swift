f7bd98087752765fe904d484d718ac3751a1a325
Kasey White <kwhitemsg@gmail.com>
2021-03-27 00:51:42 -0400

    Initial Commit

---
+++ CharityBets/CharityBetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharityBetsApp.swift
+//  CharityBets
+//
+//  Created by Kasey White on 3/27/21.
+//
+
+import SwiftUI
+
+@main
+struct CharityBetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
